faaa239fec3e15c50e1b24088cc5f189ded4cc46
tk5323-web <tk5323@srmist.edu.in>
2026-03-13 20:57:02 +0530

    UC3

--- src/HelloApp.java
+++ src/HelloApp.java
@@ -1,5 +1,6 @@
 public class HelloApp {
     public static void main(String[] args) {
-        System.out.println("Welcome to Hello App");
+        String name = (args.length > 0) ? args[0] : "World";
+        System.out.println("Hello " + name + "!");
     }
 }
