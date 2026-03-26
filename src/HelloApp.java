cee4f63d8c4dab2fffb029c5855e464b7ed88ce2
tk5323-web <tk5323@srmist.edu.in>
2026-03-26 12:35:01 +0530

    UC6

--- src/HelloApp.java
+++ src/HelloApp.java
@@ -1,13 +1,15 @@
 public class HelloApp {
     public static void main(String[] args) {
         if (args.length == 0) {
-            System.out.println("Hello World");
-        } 
-        else {
+            System.out.println("Hello, World!");
+        } else {
+            StringBuilder greeting = new StringBuilder("Hello, ");
             for (String name : args) {
-                System.out.println("Hello " + name);
+                greeting.append(name).append(", ");
             }
-
+            // Remove the trailing comma and space
+            String finalGreeting = greeting.substring(0, greeting.length() - 2);
+            System.out.println(finalGreeting + "!");
         }
     }
 }
