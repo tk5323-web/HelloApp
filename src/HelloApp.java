9d456ca31864e3c0bff37f68225f7ad703cd49d1
tk5323-web <tk5323@srmist.edu.in>
2026-03-26 12:46:38 +0530

    Update HelloApp.java

--- src/HelloApp.java
+++ src/HelloApp.java
@@ -7,7 +7,6 @@ public class HelloApp {
             for (String name : args) {
                 greeting.append(name).append(", ");
             }
-            // Remove the trailing comma and space
             String finalGreeting = greeting.substring(0, greeting.length() - 2);
             System.out.println(finalGreeting + "!");
         }
